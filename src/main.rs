ac893c9a4b3b255d53d3dd24d8d61d3331126092
wrb <wrb@wrbdeMacBook-Pro.local>
2017-03-15 20:57:03 +0800

    *fix) tmp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+// #![feature(plugin)]
+//
+// #![plugin(clippy)]
+
 pub fn main() {
     println!("gist search start.");
+    let x = Some(1u8);
+    match x {
+        Some(y) => println!("{:?}", y),
+        _ => ()
+    }
+}
+
+
+
+fn get_default3<'m,K,V:Default>(
+
+    map: &'m mut HashMap<K,V>,
+
+    key: K)
+
+    -> &'m mut V
+
+{
+
+    map.entry(key)
+
+        .or_insert_with(|| V::default())
+
 }
